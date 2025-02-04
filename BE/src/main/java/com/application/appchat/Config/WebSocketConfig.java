7588dd54cb6585c7de3d211b16b3f0c1e70a7956
dat.dotien <dat.dotien@ncc.asia>
2025-02-05 00:50:54 +0700

    Remove secret from application.properties

--- BE/src/main/java/com/application/appchat/Config/WebSocketConfig.java
+++ BE/src/main/java/com/application/appchat/Config/WebSocketConfig.java
@@ -12,7 +12,6 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-       // registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
     }
 
